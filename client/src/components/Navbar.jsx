29b25aec96b381c0520a9d9d91145dcb73fa8cc3
Chiranjeevi P <chiranjeevi.p1205@gmail.com>
2025-09-16 15:07:47 +0530

    Links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid d-flex justify-content-between align-items-center">
         <a className="navbar-brand">PARKr</a>
         <div className="d-flex gap-2">
-          <button className="btn btn-outline-success" type="button">
+          <Link to="/signup" className="btn btn-outline-success">
             Signup
-          </button>
-          <button  className="btn btn-outline-success" type="button">
+          </Link>
+          <Link to="/login" className="btn btn-outline-success">
             Login
-          </button>
+          </Link>
         </div>
       </div>
     </nav>
